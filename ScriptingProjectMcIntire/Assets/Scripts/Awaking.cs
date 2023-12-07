36a30bdf1e472cda3541fb927c1ee439339843fe
Adam McIntire <757770@student.dusd.net>
2023-12-07 14:08:28 -0800

    UpdateAndFixedUpdate

---
+++ ScriptingProjectMcIntire/Assets/Scripts/Awaking.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Awaking : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
