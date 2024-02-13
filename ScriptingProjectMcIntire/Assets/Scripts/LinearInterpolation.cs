605ba27236af58192fccef4e0f9c90015f455784
Adam McIntire <757770@student.dusd.net>
2024-02-13 14:16:07 -0800

    Kinda done with Destroy

--- ScriptingProjectMcIntire/Assets/Scripts/LinearInterpolation.cs
+++ ScriptingProjectMcIntire/Assets/Scripts/LinearInterpolation.cs
@@ -15,5 +15,13 @@ public class LiinearInterpolation : MonoBehaviour
     {
         // In this case, result = 4
         float result = Mathf.Lerp(3f, 5f, 0.5f);
+        Vector3 from = new Vector3 (1f, 2f, 3f);
+        Vector3 to = new Vector3 (5f, 6f, 7f);
+
+        // Here result = (4, 5, 6)
+        Vector3 result = Vector3.Lerp(from, to, 0.75f);
+        GetComponent<Light>().intensity = Mathf.Lerp(GetComponent<Light>().intensity, 8f, 0.5f);
+        Light.intensity = Mathf.Lerp(Light.intensity, 8f, 0.5f * Time.deltaTime);
+
     }
 }
